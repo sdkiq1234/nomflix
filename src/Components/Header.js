e651b58305115772557368e6693f7d76a7ef497f
sdkiq1234 <sdkiq123@naver.com>
2021-11-02 16:34:49 +0900

    style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const SHeader = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <ul>
-        <li>
-          <a href="/#/">Movies</a>
-        </li>
-        <li>
-          <a href="/#/tv">TV</a>
-        </li>
-        <li>
-          <a href="/#/search">Search</a>
-        </li>
-      </ul>
-    </header>
+    <SHeader>
+      <List>
+        <Item>
+          <SLink to="/#/">Movies</SLink>
+        </Item>
+        <Item>
+          <SLink to="/#/tv">TV</SLink>
+        </Item>
+        <Item>
+          <SLink to="/#/search">Search</SLink>
+        </Item>
+      </List>
+    </SHeader>
   );
 };
 
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,15 +3,19 @@ import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
 import Search from "../Routes/Search";
 import TV from "../Routes/TV";
+import Header from "./Header";
 
 const Router = () => (
   <HashRouter>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" component={TV} />
-      <Route path="/search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </HashRouter>
 );
 
